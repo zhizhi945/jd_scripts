2cc5bbe91f236626242b0d1cb5b0d9b69ceae738
zhizhi945 <77368638+zhizhi945@users.noreply.github.com>
2021-05-29 10:23:30 +0800

    Update jd_zoo.js

--- jd_zoo.js
+++ jd_zoo.js
@@ -38,8 +38,7 @@ $.inviteList = [];
 $.pkInviteList = [];
 $.secretpInfo = {};
 $.innerPkInviteList = [
-  'sSKNX-MpqKPS7Le4m5rbBpODDLhoZ9ruJViTqJpv4c2Lm2-TfJwzRBS82zBEzk4',
-  'sSKNX-MpqKOJsNvSzMSZfAM9H7GwE_7GAGP6h5-yWMFC6rsV_bSQHlBmw28L'
+  'sSKNX-MpqKOJsNu8mJ7RA9BJMup4tAAmPcPPPhBUWYKUJ19UKeC8EAoKeUXELiU',
 ];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
